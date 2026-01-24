9a6c01bc2015aed5c72dc9ddfec797f053fadeb2
dlukauskis <lukauskisdominykas@gmail.com>
2026-01-24 15:11:39 +0000

    added patched metadynamics.py to write hill heights + writing out HILLS file

--- wt-metadynamics/aladipep.py
+++ wt-metadynamics/aladipep.py
@@ -3,7 +3,7 @@ from sys import stdout
 from openmm import *
 from openmm.app import *
 from openmm.unit import kelvin, kilojoules_per_mole, picosecond
-from openmm.app.metadynamics import Metadynamics, BiasVariable
+from metadynamics import Metadynamics, BiasVariable
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,15 +21,18 @@ system = forcefield.createSystem(
 # Define collective variables for phi and psi.
 cv1 = CustomTorsionForce('theta')
 cv1.addTorsion(4, 6, 8, 14)
-phi = BiasVariable(cv1, -np.pi, np.pi, 0.35, True)
+sigma_cv0 = 0.35
+phi = BiasVariable(cv1, -np.pi, np.pi, sigma_cv0, True)
 cv2 = CustomTorsionForce('theta')
 cv2.addTorsion(6, 8, 14, 16)
-psi = BiasVariable(cv2, -np.pi, np.pi, 0.35, True)
+sigma_cv1 = 0.35
+psi = BiasVariable(cv2, -np.pi, np.pi, sigma_cv1, True)
 
 # Set up the simulation.
+bias_f = 6
 meta = Metadynamics(
     system, [phi, psi], 300.0 * kelvin,
-    6, 1.2 * kilojoules_per_mole, 100
+    bias_f, 1.2 * kilojoules_per_mole, 100
 )
 integrator = LangevinIntegrator(
     300 * kelvin, 1.0 / picosecond, 0.002 * picosecond
@@ -40,11 +43,46 @@ simulation.context.setPositions(pdb_file.positions)
 simulation.reporters.append(StateDataReporter(
     stdout, 50000, step=True,
     temperature=True,progress=True,
-    remainingTime=True,speed=True,totalSteps=500000, separator=' '))
-# TODO: add a reporter to write the biased CVs to a file + the hill heights
+    remainingTime=True,speed=True,totalSteps=500000, separator=' ')
+)
 
-# Run the simulation and plot the free energy landscape.
-meta.step(simulation, 500000)
+# TODO: write a reporter class to write the biased CVs to a file + the hill heights
+# Create PLUMED compatible HILLS file.
+file = open('HILLS','w')
+file.write('#! FIELDS time pp.proj pp.ext sigma_pp.proj sigma_pp.ext height biasf\n')
+file.write('#! SET multivariate false\n')
+file.write('#! SET kerneltype gaussian\n')
+
+# Initialise the collective variable array.
+current_cvs = np.array(list(meta.getCollectiveVariables(simulation)) + [meta.getHillHeight(simulation)])
+
+# Write the inital collective variable record.
+colvar_array = np.array([current_cvs])
+line = colvar_array[0]
+time = 0
+write_line = f'{time:15} {line[0]:20.16f} {line[1]:20.16f}          {sigma_cv0}           {sigma_cv1} {line[2]:20.16f}            {bias_f}\n'
+file.write(write_line)
+
+# Run the simulation.
+steps_per_ns = 500000
+sim_time_ns = 1
+total_steps = int(sim_time_ns * steps_per_ns)
+
+steps_per_cycle = 1000
+total_cycles = math.ceil(total_steps / steps_per_cycle)
+remaining_steps = int(sim_time_ns * steps_per_ns)
+remaining_cycles = math.ceil(remaining_steps / steps_per_cycle)
+start_cycles = total_cycles - remaining_cycles
+for cycle in range(start_cycles, total_cycles):
+    meta.step(simulation, steps_per_cycle)
+    current_cvs = np.array(list(meta.getCollectiveVariables(simulation)) + [meta.getHillHeight(simulation)])
+    colvar_array = np.append(colvar_array, [current_cvs], axis=0)
+    np.save('COLVAR.npy', colvar_array)
+    line = colvar_array[cycle + 1]
+    time = int((cycle + 1) * 0.002*steps_per_cycle)
+    write_line = f'{time:15} {line[0]:20.16f} {line[1]:20.16f}          {sigma_cv0}           {sigma_cv1} {line[2]:20.16f}            {bias_f}\n'
+    file.write(write_line)
+file.close()
 
 # TODO: flip the axes to match conventional representation of phi/psi
 # Create a contour plot of the free energy landscape.
