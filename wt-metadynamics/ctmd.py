a202b92b81415e557900b2d96a8e493d3429d1c2
dlukauskis <lukauskisdominykas@gmail.com>
2026-02-10 23:58:29 +0000

    minimal working ctmd example in openmm

--- wt-metadynamics/ctmd.py
+++ wt-metadynamics/ctmd.py
@@ -14,60 +14,10 @@ __author__ = "Dominykas Lukauskis"
 __version__ = "0.1.0"
 __email__ = "lukauskisdominykas@gmail.com"
 
-
-def main(structure = '../benchmark_systems/bCD-G1/system.gro',
-         parameters = '../benchmark_systems/bCD-G1/system.top',
-         output = 'tmp',
-         lig_resname = 'GST',
-         nreps = 1,
-         hill_height = 0.416666667):
-    """
-    args.structure : str, default='solvated.rst7'
-        Name of the structure file, either Amber or Gromacs format.
-    args.parameters : str, default='solvated.prm7'
-        Name of the parameter or topology file, either Amber or Gromacs
-        format.
-    args.output : str, default='.'
-        Path to and the name of the output directory.
-    args.lig_resname : str, default='LIG'
-        Residue name of the ligand in the structure/parameter file.
-    args.nreps : int, default=10
-        Number of repeat OpenBPMD simulations to run in series.
-    args.hill_height : float, default=0.3
-        Size of the metadynamical hill, in kcal/mol.
-    """
-    if structure.endswith('.gro'):
-        coords = GromacsGroFile(structure)
-        box_vectors = coords.getPeriodicBoxVectors()
-        parm = GromacsTopFile(parameters, periodicBoxVectors=box_vectors)
-    else:
-        coords = AmberInpcrdFile(structure)
-        parm = AmberPrmtopFile(parameters)
-
-    if not os.path.isdir(f'{output}'):
-        os.mkdir(f'{output}')
-
-    # Run NREPS number of production simulations
-    for idx in range(0, nreps):
-        rep_dir = os.path.join(output, f'rep_{idx}')
-        if not os.path.isdir(rep_dir):
-            os.mkdir(rep_dir)
-
-        if os.path.isfile(os.path.join(rep_dir, 'ctmd_results.csv')):
-            continue
-
-        produce(
-            coords, parm, output, idx, lig_resname, hill_height,
-            anchor_atom_lst=[0,21,42,63,84,105,126],
-        )
-
-
-    return None
-
-
+# TODO: move this function to metadynamics.py
 def compute_ct(s_grid, V_bias, beta, gamma):
     """
-    Compute c(t) from equation 1 (Tiwary & Parrinello 2015)
+    Compute c(t) from equation 1 (https://www.biorxiv.org/content/10.64898/2026.02.05.703972v1.full.pdf)
 
     Parameters:
     -----------
@@ -91,139 +41,159 @@ def compute_ct(s_grid, V_bias, beta, gamma):
     # Denominator: integral of exp[1/(gamma-1) * beta*V(s,t)]
     denominator = np.trapezoid(np.exp(1 / (gamma - 1) * beta * V_bias), s_grid)
 
-    # c(t) = (1/beta) * log(numerator / denominator)
     c_t = (1 / beta) * np.log(numerator / denominator)
 
     return c_t
 
+"""
+structure : str
+    Name of the structure file, either Amber or Gromacs format.
+parameters : str
+    Name of the parameter or topology file, either Amber or Gromacs
+    format.
+output : str
+    Path to and the name of the output directory.
+lig_resname
+    Residue name of the ligand in the structure/parameter file.
+hill_height : float, default=1.75
+    Size of the metadynamical hill, in kJ/mol.
+"""
 
-def produce(input_pos, parm, out_dir, idx, lig_resname, set_hill_height, anchor_atom_lst: list = []):
-    """An CTMD production simulation function. Ligand RMSD is biased with
-    wt-metadynamics. The integrator uses a 4 fs time step and
-    runs for 5 ns, writing a frame every 100 ps.
+structure = '../benchmark_systems/bCD-G1/system.gro'
+parameters = '../benchmark_systems/bCD-G1/system.top'
+out_dir_name, idx = 'tmp', 0
+lig_resname = 'GST'
+set_hill_height = 1.75
+anchor_atom_lst = [0,21,42,63,84,105,126]  # for proteins, this can be the CA atoms near protein CoG
+in_vacuo = True  # only for testing purposes with the host-guest system
+sim_time = 5  # ns
+deposition_pace = 250  # deposit a hill every 1 ps, so 250 steps with a 4 fs timestep
 
-    Writes a 'trj.dcd', 'COLVAR.npy', 'bias_*.npy' and 'sim_log.csv' files
-    during the metadynamics simulation in the '{out_dir}/rep_{idx}' directory.
-    After the simulation is done, it analyses the trajectories and writes a
-    'ctmd_results.csv' file.
 
-    Parameters
-    ----------
-    input_pos : AmberInpcrdFile or GromacsGroFile object
-        Name of the PDB for equilibrated system.
-    parm : Parmed or OpenMM parameter file object
-        Used to create the OpenMM System object.
-    out_dir : str
-        Directory where your equilibration PDBs and 'rep_*' dirs are at.
-    idx : int
-        Current replica index.
-    lig_resname : str
-        Residue name of the ligand.
-    set_hill_height : float
-        Metadynamic hill height, in kcal/mol.
-    anchor_atom_lst : list, default=[]
-        List of atom indices to use as anchor atoms for the RMSD calculation.
-    """
-    # First, assign the replica directory to which we'll write the files
-    write_dir = os.path.join(out_dir, f'rep_{idx}')
+if structure.endswith('.gro'):
+    coords = GromacsGroFile(structure)
+    box_vectors = coords.getPeriodicBoxVectors()
+    parm = GromacsTopFile(parameters, periodicBoxVectors=box_vectors)
+else:
+    coords = AmberInpcrdFile(structure)
+    parm = AmberPrmtopFile(parameters)
 
-    lig_ha_lst = [
-        atom.index for atom in parm.topology.atoms()
-        if atom.residue.name == lig_resname and not atom.name.startswith("H")
-    ]
+# First, assign the replica directory to which we'll write the files
+out_dir_path = os.path.join(out_dir_name, f'rep_{idx}')
+os.makedirs(out_dir_path, exist_ok=True)
 
-    # Set up the system to run metadynamics
+lig_ha_lst = [
+    atom.index for atom in parm.topology.atoms()
+    if atom.residue.name == lig_resname and not atom.name.startswith("H")
+]
+
+# Set up the system to run metadynamics
+if in_vacuo:
     system = parm.createSystem(
         nonbondedMethod=NoCutoff,
         constraints=HBonds,
         hydrogenMass=4 * amu,
     )
-    # get the atom positions for the system from the equilibrated
-    # system
-    input_positions = input_pos.getPositions()
-
-    # Add an 'empty' flat-bottom restraint to fix the issue with PBC.
-    # Without one, RMSDForce object fails to account for PBC.
-    k = 0 * kilojoules_per_mole  # NOTE - 0 kJ/mol constant
-    upper_wall = 10.00 * nanometer
-    fb_eq = '(k/2)*max(distance(g1,g2) - upper_wall, 0)^2'
-    upper_wall_rest = CustomCentroidBondForce(2, fb_eq)
-    upper_wall_rest.addGroup(lig_ha_lst)
-    upper_wall_rest.addGroup(anchor_atom_lst)
-    upper_wall_rest.addBond([0, 1])
-    upper_wall_rest.addGlobalParameter('k', k)
-    upper_wall_rest.addGlobalParameter('upper_wall', upper_wall)
-    upper_wall_rest.setUsesPeriodicBoundaryConditions(True)
-    system.addForce(upper_wall_rest)
-
-    alignment_indices = lig_ha_lst + anchor_atom_lst
-
-    rmsd = RMSDForce(input_positions, alignment_indices)
-    # Set up the typical metadynamics parameters
-    grid_min, grid_max = 0.0, 1.0  # nm
-    hill_height = set_hill_height * kilocalories_per_mole
-    hill_width = 0.015  # nm, also known as sigma
-
-    grid_width = hill_width / 5
-    # 'grid' here refers to the number of grid points
-    grid = int(abs(grid_min - grid_max) / grid_width)
-
-    rmsd_cv = BiasVariable(rmsd, grid_min, grid_max, hill_width,
-                           False, gridWidth=grid)
-
-    # define the metadynamics object
-    # deposit bias every 1 ps, BF = 10, write bias every ns
-    meta = Metadynamics(
-        system, [rmsd_cv], 300.0 * kelvin, 10.0, hill_height,250,
-        biasDir=write_dir, saveFrequency=250000
+else:
+    system = parm.createSystem(
+        nonbondedMethod=PME,
+        nonbondedCutoff=1.0 * nanometer,
+        constraints=HBonds,
+        hydrogenMass=4 * amu,
     )
 
-    # Set up and run metadynamics
-    integrator = LangevinIntegrator(300 * kelvin, 1.0 / picosecond,
-                                    0.004 * picosecond)
+# get the atom positions for the system from the equilibrated
+# system
+input_positions = coords.getPositions()
+
+# Add an 'empty' flat-bottom restraint to fix the issue with PBC.
+# Without one, RMSDForce object fails to account for PBC.
+k = 0 * kilojoules_per_mole  # NOTE - 0 kJ/mol constant
+upper_wall = 10.00 * nanometer
+fb_eq = '(k/2)*max(distance(g1,g2) - upper_wall, 0)^2'
+upper_wall_rest = CustomCentroidBondForce(2, fb_eq)
+upper_wall_rest.addGroup(lig_ha_lst)
+upper_wall_rest.addGroup(anchor_atom_lst)
+upper_wall_rest.addBond([0, 1])
+upper_wall_rest.addGlobalParameter('k', k)
+upper_wall_rest.addGlobalParameter('upper_wall', upper_wall)
+upper_wall_rest.setUsesPeriodicBoundaryConditions(True)
+system.addForce(upper_wall_rest)
+
+alignment_indices = lig_ha_lst + anchor_atom_lst
+
+# OpenMM RMSD CV is not the same as the one in PLUMED, but it'll do
+rmsd = RMSDForce(input_positions, alignment_indices)
+# Set up the typical metadynamics parameters
+grid_min, grid_max = 0.0, 1.0  # in nm
+hill_height = set_hill_height * kilojoules_per_mole
+hill_width = 0.015  # in nm, aka sigma
+
+grid_width = hill_width / 5
+# 'grid' here refers to the number of grid points
+grid = int(abs(grid_min - grid_max) / grid_width)
+
+rmsd_cv = BiasVariable(rmsd, grid_min, grid_max, hill_width,
+                       False, gridWidth=grid)
+
+# define the metadynamics object, deposit bias every 1 ps, BF = 10, write bias every ns
+meta = Metadynamics(
+    system, [rmsd_cv], 300.0 * kelvin, 10.0, hill_height, deposition_pace,
+    biasDir=out_dir_path, saveFrequency=250000
+)
+
+# Set up and run metadynamics
+integrator = LangevinIntegrator(300 * kelvin, 1.0 / picosecond, 0.004 * picosecond)
+
+if in_vacuo:
     platform = Platform.getPlatformByName('CPU')
-    # properties = {'CudaPrecision': 'mixed'}
     properties = {}
+else:
+    platform = Platform.getPlatformByName('CUDA')
+    properties = {'CudaPrecision': 'mixed'}
 
-    simulation = Simulation(parm.topology, system, integrator, platform,
-                            properties)
-    simulation.context.setPositions(input_positions)
+simulation = Simulation(parm.topology, system, integrator, platform, properties)
+simulation.context.setPositions(input_positions)
 
-    trj_name = os.path.join(write_dir, 'trj.dcd')
+trj_fpath = os.path.join(out_dir_path, 'trj.dcd')
+sim_log_fpath = os.path.join(out_dir_path, 'sim_log.csv')
 
-    sim_time = 5  # ns
-    steps = 250000 * sim_time
+total_steps = 250000 * sim_time
 
-    simulation.reporters.append(DCDReporter(trj_name, 25000))  # every 100 ps
-    simulation.reporters.append(StateDataReporter(
-        os.path.join(write_dir, 'sim_log.csv'), 250000,
-        step=True, temperature=True, progress=True,
-        remainingTime=True, speed=True,
-        totalSteps=steps, separator=','))  # every 1 ns
+simulation.reporters.append(DCDReporter(trj_fpath, 25000))  # every 100 ps
+simulation.reporters.append(StateDataReporter(
+    sim_log_fpath, 250000,
+    step=True, temperature=True, progress=True,
+    remainingTime=True, speed=True,
+    totalSteps=total_steps, separator=','))  # every 1 ns
 
-    colvar_array = np.array([meta.getCollectiveVariables(simulation)])
-    # calculate the c(t) as described in Eq 1. of https://www.biorxiv.org/content/10.64898/2026.02.05.703972v1.full.pdf
+colvar_fpath = os.path.join(out_dir_path, 'COLVAR.npy')
 
-    # Create grid for 1D bias
-    cv = meta.variables[0]  # RMSD is the only CV
-    s_grid = np.linspace(cv.minValue, cv.maxValue, meta._totalBias.shape[0])
+# Create grid for 1D bias
+cv = meta.variables[0]
+s_grid = np.linspace(cv.minValue, cv.maxValue, meta._totalBias.shape[0])
+c_t = compute_ct(s_grid, meta._totalBias, 1 / (0.008314 * 300), meta.biasFactor)
 
+starting_cv = meta.getCollectiveVariables(simulation)[0]
+colvar_array = np.array([starting_cv, c_t])
+
+ps_spent_above_6_ang = 0
+for i in range(0, int(total_steps), deposition_pace):
     c_t = compute_ct(s_grid, meta._totalBias, 1 / (0.008314 * 300), meta.biasFactor)
-    print(f"Initial c(t): {c_t:.2f} kJ/mol")
-    for i in range(0, int(steps), 500):
-        c_t = compute_ct(s_grid, meta._totalBias, 1 / (0.008314 * 300), meta.biasFactor)
-        print(f"step {i}: c(t): {c_t:.2f} kJ/mol")
-        if i % 25000 == 0:
-            # log the stored COLVAR every 100ps
-            np.save(os.path.join(write_dir, 'COLVAR.npy'), colvar_array)
-        meta.step(simulation, 500)
-        current_cvs = meta.getCollectiveVariables(simulation)
-        # record the CVs every 2 ps
-        colvar_array = np.append(colvar_array, [current_cvs], axis=0)
-    np.save(os.path.join(write_dir, 'COLVAR.npy'), colvar_array)
+    if i % 25000 == 0:
+        # log the stored COLVAR every 100ps
+        np.save(colvar_fpath, colvar_array)
+    meta.step(simulation, deposition_pace)
+    current_cv = meta.getCollectiveVariables(simulation)[0]
+    if current_cv > 0.6:  # 0.6 nm = 6 angstroms
+        ps_spent_above_6_ang += 1
 
-    return None
+    # record the CVs every 1 ps
+    colvar_array = np.vstack([colvar_array, np.array([current_cv, c_t])])
 
+    # if more than 200 ps are spent above 6 angstroms, stop the simulation according to protocol
+    if ps_spent_above_6_ang > 200:
+        break
 
-if __name__ == '__main__':
-    main()
+# save the final COLVAR array to file
+np.save(colvar_fpath, colvar_array)
